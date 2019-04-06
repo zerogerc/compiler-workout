435f13670261b1b85a03edd5c2546c6f4d80eb7b
danyaberezun <danya.berezun@gmail.com>
2019-04-06 21:50:21 +0300

    hw8

---
+++ src/Language.ml
@@ -0,0 +1,248 @@
+(* Opening a library for generic programming (https://github.com/dboulytchev/GT).
+   The library provides "@type ..." syntax extension and plugins like show, etc.
+*)
+open GT
+
+(* Opening a library for combinator-based syntax analysis *)
+open Ostap
+open Combinators
+
+(* Values *)
+module Value =
+  struct
+
+    @type t = Int of int | String of string | Array of t list with show
+
+    let to_int = function 
+    | Int n -> n 
+    | _ -> failwith "int value expected"
+
+    let to_string = function 
+    | String s -> s 
+    | _ -> failwith "string value expected"
+
+    let to_array = function
+    | Array a -> a
+    | _       -> failwith "array value expected"
+
+    let of_int    n = Int    n
+    let of_string s = String s
+    let of_array  a = Array  a
+
+    let update_string s i x = String.init (String.length s) (fun j -> if j = i then x else s.[j])
+    let update_array  a i x = List.init   (List.length a)   (fun j -> if j = i then x else List.nth a j)
+
+  end
+       
+(* States *)
+module State =
+  struct
+                                                                
+    (* State: global state, local state, scope variables *)
+    type t = {g : string -> Value.t; l : string -> Value.t; scope : string list}
+
+    (* Empty state *)
+    let empty =
+      let e x = failwith (Printf.sprintf "Undefined variable: %s" x) in
+      {g = e; l = e; scope = []}
+
+    (* Update: non-destructively "modifies" the state s by binding the variable x 
+       to value v and returns the new state w.r.t. a scope
+    *)
+    let update x v s =
+      let u x v s = fun y -> if x = y then v else s y in
+      if List.mem x s.scope then {s with l = u x v s.l} else {s with g = u x v s.g}
+
+    (* Evals a variable in a state w.r.t. a scope *)
+    let eval s x = (if List.mem x s.scope then s.l else s.g) x
+
+    (* Creates a new scope, based on a given state *)
+    let enter st xs = {empty with g = st.g; scope = xs}
+
+    (* Drops a scope *)
+    let leave st st' = {st' with g = st.g}
+
+  end
+
+(* Builtins *)
+module Builtin =
+  struct
+
+    let eval (st, i, o, _) args = function
+    | "read"     -> (match i with z::i' -> (st, i', o, Some (Value.of_int z)) | _ -> failwith "Unexpected end of input")
+    | "write"    -> (st, i, o @ [Value.to_int @@ List.hd args], None)
+    | "$elem"    -> let [b; j] = args in
+                    (st, i, o, let i = Value.to_int j in
+                               Some (match b with
+                                     | Value.String s -> Value.of_int @@ Char.code s.[i]
+                                     | Value.Array  a -> List.nth a i
+                               )
+                    )         
+    | "$length"  -> (st, i, o, Some (Value.of_int (match List.hd args with Value.Array a -> List.length a | Value.String s -> String.length s)))
+    | "$array"   -> (st, i, o, Some (Value.of_array args))
+    | "isArray"  -> let [a] = args in (st, i, o, Some (Value.of_int @@ match a with Value.Array  _ -> 1 | _ -> 0))
+    | "isString" -> let [a] = args in (st, i, o, Some (Value.of_int @@ match a with Value.String _ -> 1 | _ -> 0))                     
+       
+  end
+    
+(* Simple expressions: syntax and semantics *)
+module Expr =
+  struct
+    
+    (* The type for expressions. Note, in regular OCaml there is no "@type..." 
+       notation, it came from GT. 
+    *)
+    @type t =
+    (* integer constant   *) | Const  of int
+    (* array              *) | Array  of t list
+    (* string             *) | String of string
+    (* S-expressions      *) | Sexp   of string * t list
+    (* variable           *) | Var    of string
+    (* binary operator    *) | Binop  of string * t * t
+    (* element extraction *) | Elem   of t * t
+    (* length             *) | Length of t 
+    (* function call      *) | Call   of string * t list with show
+
+    (* Available binary operators:
+        !!                   --- disjunction
+        &&                   --- conjunction
+        ==, !=, <=, <, >=, > --- comparisons
+        +, -                 --- addition, subtraction
+        *, /, %              --- multiplication, division, reminder
+    *)
+
+    (* The type of configuration: a state, an input stream, an output stream, an optional value *)
+    type config = State.t * int list * int list * Value.t option
+                                                            
+    (* Expression evaluator
+
+          val eval : env -> config -> t -> int * config
+
+
+       Takes an environment, a configuration and an expresion, and returns another configuration. The 
+       environment supplies the following method
+
+           method definition : env -> string -> int list -> config -> config
+
+       which takes an environment (of the same type), a name of the function, a list of actual parameters and a configuration, 
+       an returns a pair: the return value for the call and the resulting configuration
+    *)                                                       
+    let rec eval env ((st, i, o, r) as conf) expr = failwith "Not implemented"
+    and eval_list env conf xs =
+      let vs, (st, i, o, _) =
+        List.fold_left
+          (fun (acc, conf) x ->
+             let (_, _, _, Some v) as conf = eval env conf x in
+             v::acc, conf
+          )
+          ([], conf)
+          xs
+      in
+      (st, i, o, List.rev vs)
+         
+    (* Expression parser. You can use the following terminals:
+
+         IDENT   --- a non-empty identifier a-zA-Z[a-zA-Z0-9_]* as a string
+         DECIMAL --- a decimal constant [0-9]+ as a string                                                                                                                  
+    *)
+    ostap (                                      
+      parse: empty {failwith "Not implemented"}
+    )
+    
+  end
+                    
+(* Simple statements: syntax and sematics *)
+module Stmt =
+  struct
+
+    (* The type for statements *)
+    type t =
+    (* assignment                       *) | Assign of string * Expr.t list * Expr.t
+    (* composition                      *) | Seq    of t * t 
+    (* empty statement                  *) | Skip
+    (* conditional                      *) | If     of Expr.t * t * t
+    (* loop with a pre-condition        *) | While  of Expr.t * t
+    (* loop with a post-condition       *) | Repeat of t * Expr.t
+    (* return statement                 *) | Return of Expr.t option
+    (* call a procedure                 *) | Call   of string * Expr.t list
+                                                                    
+    (* Statement evaluator
+
+         val eval : env -> config -> t -> config
+
+       Takes an environment, a configuration and a statement, and returns another configuration. The 
+       environment is the same as for expressions
+    *)
+
+    let update st x v is =
+      let rec update a v = function
+      | []    -> v           
+      | i::tl ->
+          let i = Value.to_int i in
+          (match a with
+           | Value.String s when tl = [] -> Value.String (Value.update_string s i (Char.chr @@ Value.to_int v))
+           | Value.Array a               -> Value.Array  (Value.update_array  a i (update (List.nth a i) v tl))
+          ) 
+      in
+      State.update x (match is with [] -> v | _ -> update (State.eval st x) v is) st
+          
+    let rec eval env ((st, i, o, r) as conf) k stmt = failwith "Not implemented"
+         
+    (* Statement parser *)
+    ostap (
+      parse: empty {failwith "Not implemented"}
+    )
+      
+  end
+
+(* Function and procedure definitions *)
+module Definition =
+  struct
+
+    (* The type for a definition: name, argument list, local variables, body *)
+    type t = string * (string list * string list * Stmt.t)
+
+    ostap (
+      arg  : IDENT;
+      parse: %"fun" name:IDENT "(" args:!(Util.list0 arg) ")"
+         locs:(%"local" !(Util.list arg))?
+        "{" body:!(Stmt.parse) "}" {
+        (name, (args, (match locs with None -> [] | Some l -> l), body))
+      }
+    )
+
+  end
+    
+(* The top-level definitions *)
+
+(* The top-level syntax category is a pair of definition list and statement (program body) *)
+type t = Definition.t list * Stmt.t    
+
+(* Top-level evaluator
+
+     eval : t -> int list -> int list
+
+   Takes a program and its input stream, and returns the output stream
+*)
+let eval (defs, body) i =
+  let module M = Map.Make (String) in
+  let m          = List.fold_left (fun m ((name, _) as def) -> M.add name def m) M.empty defs in  
+  let _, _, o, _ =
+    Stmt.eval
+      (object
+         method definition env f args ((st, i, o, r) as conf) =
+           try
+             let xs, locs, s      =  snd @@ M.find f m in
+             let st'              = List.fold_left (fun st (x, a) -> State.update x a st) (State.enter st (xs @ locs)) (List.combine xs args) in
+             let st'', i', o', r' = Stmt.eval env (st', i, o, r) Stmt.Skip s in
+             (State.leave st'' st, i', o', r')
+           with Not_found -> Builtin.eval conf args f
+       end)
+      (State.empty, i, [], None)
+      Stmt.Skip
+      body
+  in
+  o
+
+(* Top-level parser *)
+let parse = ostap (!(Definition.parse)* !(Stmt.parse))
---
+++ src/SM.ml
@@ -0,0 +1,84 @@
+open GT       
+open Language
+       
+(* The type for the stack machine instructions *)
+@type insn =
+(* binary operator                 *) | BINOP   of string
+(* put a constant on the stack     *) | CONST   of int
+(* put a string on the stack       *) | STRING  of string                      
+(* load a variable to the stack    *) | LD      of string
+(* store a variable from the stack *) | ST      of string
+(* store in an array               *) | STA     of string * int
+(* a label                         *) | LABEL   of string
+(* unconditional jump              *) | JMP     of string
+(* conditional jump                *) | CJMP    of string * string
+(* begins procedure definition     *) | BEGIN   of string * string list * string list
+(* end procedure definition        *) | END
+(* calls a function/procedure      *) | CALL    of string * int * bool
+(* returns from a function         *) | RET     of bool with show
+                                                   
+(* The type for the stack machine program *)
+type prg = insn list
+                            
+(* The type for the stack machine configuration: control stack, stack and configuration from statement
+   interpreter
+*)
+type config = (prg * State.t) list * Value.t list * Expr.config
+
+(* Stack machine interpreter
+
+     val eval : env -> config -> prg -> config
+
+   Takes an environment, a configuration and a program, and returns a configuration as a result. The
+   environment is used to locate a label to jump to (via method env#labeled <label_name>)
+*)
+let split n l =
+  let rec unzip (taken, rest) = function
+  | 0 -> (List.rev taken, rest)
+  | n -> let h::tl = rest in unzip (h::taken, tl) (n-1)
+  in
+  unzip ([], l) n
+        
+let rec eval env ((cstack, stack, ((st, i, o) as c)) as conf) prg = failwith "Not implemented"
+
+(* Top-level evaluation
+
+     val run : prg -> int list -> int list
+
+   Takes a program, an input stream, and returns an output stream this program calculates
+*)
+let run p i =
+  let module M = Map.Make (String) in
+  let rec make_map m = function
+  | []              -> m
+  | (LABEL l) :: tl -> make_map (M.add l tl m) tl
+  | _ :: tl         -> make_map m tl
+  in
+  let m = make_map M.empty p in
+  let (_, _, (_, _, o)) =
+    eval
+      (object
+         method is_label l = M.mem l m
+         method labeled l = M.find l m
+         method builtin (cstack, stack, (st, i, o)) f n p =
+           let f = match f.[0] with 'L' -> String.sub f 1 (String.length f - 1) | _ -> f in
+           let args, stack' = split n stack in
+           let (st, i, o, r) = Language.Builtin.eval (st, i, o, None) (List.rev args) f in
+           let stack'' = if p then stack' else let Some r = r in r::stack' in
+           Printf.printf "Builtin: %s\n";
+           (cstack, stack'', (st, i, o))
+       end
+      )
+      ([], [], (State.empty, i, []))
+      p
+  in
+  o
+
+(* Stack machine compiler
+
+     val compile : Language.t -> prg
+
+   Takes a program in the source language and returns an equivalent program for the
+   stack machine
+*)
+let compile (defs, p) = failwith "Not implemented"
